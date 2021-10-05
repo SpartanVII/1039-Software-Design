9541c44fe843c556dcf53a8763c27a3f2efa8ff2
SpartanVII <jonpitarchmartinez@gmail.com>
2021-10-05 16:38:02 +0200

    V1.1

---
+++ src/HW3/EuropeChefKnife.java
@@ -0,0 +1,7 @@
+package HW3;
+
+public class EuropeChefKnife extends Knife {
+    public EuropeChefKnife() {
+        name = "Europe Chefs knife";
+    }
+}
---
+++ src/HW3/EuropeSteakKnife.java
@@ -0,0 +1,7 @@
+package HW3;
+
+public class EuropeSteakKnife extends Knife {
+    public EuropeSteakKnife() {
+        name = "Europe Steak knife";
+    }
+}
